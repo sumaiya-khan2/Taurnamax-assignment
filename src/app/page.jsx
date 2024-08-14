091f528435a416661734168d3a9d709a57c5d580
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-15 05:46:31 +0600

    started the basic project with basic installation

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        {/* div for the bar */}
+        <div>
+          <h3>Tournamax Assignment</h3>
+          <button>Add topic!!!</button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
