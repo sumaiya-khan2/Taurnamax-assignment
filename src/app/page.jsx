df6797fb17992eede1a036ca13db7473e024e7b9
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-15 06:00:30 +0600

    created the navbar of the project

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,10 @@
-import React from 'react';
+import Navbar from "@/components/Navbar";
 
 const Home = () => {
   return (
     <>
-      <div>
-        {/* div for the bar */}
-        <div>
-          <h3>Tournamax Assignment</h3>
-          <button>Add topic!!!</button>
-        </div>
+      <div className="max-w-screen-md mx-auto">
+        <Navbar />
       </div>
     </>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+const Navbar = () => {
+    return (
+        <>
+            <div className="bg-[#1E293B] mt-4 flex justify-between items-center">
+                <h3 className="text-white text-3xl font-semibold p-4">Tournamax Assignment</h3>
+                <button className="bg-white text-black me-5 rounded-lg p-2">Add topic</button>
+            </div>
+        </>
+    );
+};
+
+export default Navbar;
